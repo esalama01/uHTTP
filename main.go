8257b11310c18e0232ffd1a3d6f46e65e3aedc69
esalama01 <mehdisalama61@gmail.com>
2026-09-18 03:45:12 +0100

    shit is too hard

--- main.go
+++ main.go
@@ -1,2 +1,22 @@
-package uhttp
-// client <==> [server](PROXY)[client] <==> server
+package main
+
+import(
+	//"golang.org/x/sys/unix"
+	//"log"
+	"fmt"
+	"time"
+)
+
+func main(){
+	/*
+	fd, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM,0)
+	if err != nil {
+		log.Fatalf("Failed to create socket: %v", err)
+	}
+	defer unix.Close(fd)
+	*/
+	for {
+		fmt.Println("Listening for incoming conn..")
+		time.Sleep(1 * time.Second)
+	}
+}
--- src/server.go
+++ src/server.go
@@ -1,31 +1,20 @@
 package src
 
 import(
-	"net"
+	"golang.org/x/sys/unix"
 	"log"
 	"fmt"
+	"time"
 )
 
-func StartServer(){
-	for{//an infinite loop to listen to any upcoming connection at anytime
-		listener, err := net.Listen("tcp", "127.0.1.1:8080") // localhost <=> my laptop acting as my own server
-		if err != nil{
-			log.Fatal(err)
-		}
-		defer listener.Close() //ensures the port gets freed when the function returns
-		for {
-			client, err := listener.Accept()
-			if err != nil {
-				log.Fatal(err)
-				continue
-			}
-			HandleConnection(client)
-		}
+func main(){
+	fd, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM,0)
+	if err != nil {
+		log.Fatalf("Failed to create socket: %v", err)
 	}
-}
-
-func HandleConnection(conn net.Conn){
-	fmt.Printf("Redirecting into %s\n", conn.RemoteAddr().String())
-	defer conn.Close()
-
+	defer unix.Close(fd)
+	for {
+		fmt.Println("Listening for incoming conn..")
+		time.Sleep(1 * time.Second)
+	}
 }
