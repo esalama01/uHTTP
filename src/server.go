d28ef4586762c5c11ef011ddb137b26fe5bb142a
esalama01 <mehdisalama61@gmail.com>
2026-09-21 18:59:44 +0000

    started listening on port 8989

--- src/server.go
+++ src/server.go
@@ -1,20 +1,48 @@
-package src
+package main
 
 import(
-	"golang.org/x/sys/unix"
-	"log"
+	"net"
 	"fmt"
-	"time"
+	"bufio"
+	"io"
 )
 
 func main(){
-	fd, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM,0)
-	if err != nil {
-		log.Fatalf("Failed to create socket: %v", err)
+	listener, err := net.Listen("tcp", ":8989")
+	if err != nil{
+		fmt.Println("Error Listening: ",err)
+		return
 	}
-	defer unix.Close(fd)
+	defer listener.Close()
+	fmt.Println("Server Running On Port 8989..")
 	for {
-		fmt.Println("Listening for incoming conn..")
-		time.Sleep(1 * time.Second)
+		conn, err := listener.Accept()
+		if err != nil{
+			fmt.Println("Error Accepting: ",err)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn){
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	for {
+		bytes, err := reader.ReadBytes(byte('\n'))
+		if err != nil{
+			if err != io.EOF {
+				fmt.Println("Failed to read the data.")
+			}
+			return
+		}
+		fmt.Printf("request: %s", bytes)
+		line := fmt.Sprintf("Echo: %s", bytes)
+		fmt.Printf("response: %s", line)
+		_, err = conn.Write([]byte(line))
+		if err != nil{
+			fmt.Println("Failed to write the data.")
+			return
+		}
 	}
 }
